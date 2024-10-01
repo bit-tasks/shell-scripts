b99f9e7335d686d4ce700eba40c69880475bf91a
Ashan Fernando <ashan256@gmail.com>
2024-10-01 17:39:52 +0530

    Adding support for soft delete

--- scripts/lane-cleanup.sh
+++ scripts/lane-cleanup.sh
@@ -6,11 +6,20 @@ SCOPE="$2"
 LANE="$3"
 WSDIR="$4"
 
+# BIT_CONFIG_ACCESS_TOKEN OR BIT_CONFIG_USER_TOKEN
+BIT_TOKEN="$5" 
+
 # change to working directory before running the remaining commands
 cd "${WSDIR}"
 
-# try to remove the bit lane
+# Option 1: Hard Delete
 bit lane remove "${ORG}.${SCOPE}/${LANE}" --remote --silent --force 2> /dev/null
 if [ $? -ne 0 ]; then
     echo "Error while removing bit lane. Lane may not exist"
 fi
+
+# Option 2: Soft Delete
+curl -X POST "https://api.v2.bit.cloud/graphql" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $BIT_TOKEN" \
+-d '{"query": "mutation DELETE_LANE($id: String!) { deleteLane(id: $id) }", "variables": {"id": "'"${ORG}.${SCOPE}/${LANE}"'"}}'
